504bc283c68f95b7d3133928495f3a543db867ae
mansi Sinha <manshi0832@gmail.com>
2026-05-18 19:02:59 +0530

    Added search UI layout

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn=document.getElementById("submitSearch");
+let result=document.getElementById("result");
+result.style.display="none";
+btn.addEventListener("click",function(){
+result.style.display="block";
+});
