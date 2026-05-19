dc152714612e81897ce2e2e08d7c6efd7b1b7c0c
mansi Sinha <manshi0832@gmail.com>
2026-05-19 22:51:51 +0530

    Added input handling and click event

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let btn=document.getElementById("submitSearch");
 let result=document.getElementById("result");
 result.style.display="none";
+
+let text=document.getElementById("searchTxt");
 btn.addEventListener("click",function(){
+console.log(text.value);
 result.style.display="block";
+result.textContent=text.value;
 });
